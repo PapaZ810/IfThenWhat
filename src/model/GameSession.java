1f3992e233027fa19cb6c8904daffc6b3a98a925
Zac <zombiezac@att.net>
2018-09-20 12:03:50 -0600

    added space and a default constructor

---
+++ src/model/GameSession.java
@@ -0,0 +1,20 @@
+package model;
+
+public class GameSession
+{
+	private int timeMinutes, timeHours, inGameKills;
+	private String name;
+	
+	
+	public GameSession()
+	{
+		
+	}
+
+	
+	
+	
+	
+
+}
+
