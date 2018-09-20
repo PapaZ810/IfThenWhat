7bbb82ea81b7761b0e86f4600f427b38a2793902
Zac <zombiezac@att.net>
2018-09-20 12:30:16 -0600

    added getters and setters

--- src/model/GameSession.java
+++ src/model/GameSession.java
@@ -5,16 +5,65 @@ public class GameSession
 	private int timeMinutes, timeHours, inGameKills;
 	private String name;
 	
-	
+	/**
+	 * default constructor for GameSession
+	 * init all variables to valid but bad values
+	 * used for later customization
+	 */
+
 	public GameSession()
 	{
-		
+		this.name = "unnamed game session";
+		this.timeMinutes = -99;
+		this.timeHours = -99;
+		this.inGameKills = -99;
 	}
-
+	
+	public GameSession(String name)
+	{
+		this.name = name;
+	}
+	
+	public int getTimeMinutes()
+	{
+		return timeMinutes;
+	}
+	
+	public int getTimeHours()
+	{
+		return timeHours;
+	}
+	
+	public int getInGameKills()
+	{
+		return inGameKills;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setTimeMinutes(int timeMinutes)
+	{
+		this.timeMinutes = timeMinutes;
+	}
+	
+	public void setTimeHours(int timeHours)
+	{
+		this.timeHours = timeHours;
+	}
+	
+	public void setInGameKills(int inGameKills)
+	{
+		this.inGameKills = inGameKills;
+	}
 	
 	
-	
-	
-
 }
 
